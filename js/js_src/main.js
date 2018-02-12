ccf3941ef7460d25589d1ddc29dc78433b3911b2
Sean Hallahan <sfhallahan@gmail.com>
2018-02-11 20:19:45 -0500

    refactored to main.js to remove jQuery

--- js/js_src/main.js
+++ js/js_src/main.js
@@ -1,59 +1,66 @@
 // open and close navbar for mobile
-var isOpen = false;
-
-function closeNav() {
-  $("#nav-container").removeClass("open").addClass("closed");
-  $("#toggle-navigation").removeClass("open").addClass("closed");
-  $("#toggle-navigation").attr("aria-expanded", "false");
-  isOpen = false;
-}
-
-function openNav() {
-  $("#nav-container").removeClass("closed").addClass("open");
-  $("#toggle-navigation").removeClass("closed").addClass("open");
-  $("#toggle-navigation").attr("aria-expanded", "true");
-  isOpen = true;
-}
-
-$(document).ready(function () {
-  $("#toggle-navigation").click(function() {
-    isOpen == true ? closeNav() : openNav();
-  });
-});
-
-
-// Hide Header on on scroll down
-
 var didScroll;
-var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = 98; //$('header').outerHeight();
+var navbarHeight = 98;
 
-$(window).scroll(function(event){
+window.onscroll = function(event) {
     didScroll = true;
-});
+};
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    if (st > lastScrollTop && st > navbarHeight){
-        $('header').removeClass('nav-down').addClass('nav-up');
-        closeNav();
-    } else {
-        if(st + $(window).height() < $(document).height()) {
-          $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-
-    lastScrollTop = st;
+function NavBar() {
+  this.isOpen = false;
+  this.navContainer = document.getElementById('nav-container');
+  this.toggleNavigationBtn = document.getElementById('toggle-navigation');
+  this.header = document.getElementById('header');
+  this.lastScrollTop = 0;
+  console.log('toggle nav created')
 }
+
+NavBar.prototype.closeNav = function() {
+  this.navContainer.classList.replace('open', 'closed');
+  this.toggleNavigationBtn.classList.replace('open', 'closed');
+  this.toggleNavigationBtn.setAttribute('aria-expanded', 'false');
+  this.isOpen = false;
+}
+
+NavBar.prototype.openNav = function() {
+  this.navContainer.classList.replace('closed', 'open');
+  this.toggleNavigationBtn.classList.replace('closed', 'open');
+  this.toggleNavigationBtn.setAttribute('aria-expanded', 'true');
+  this.isOpen = true;
+}
+
+NavBar.prototype.handleScroll = function() {
+  var st = window.scrollY;
+
+  if(Math.abs(this.lastScrollTop - st) <= delta)
+      return;
+
+  if (st > this.lastScrollTop && st > navbarHeight){
+      this.header.classList.replace('nav-down', 'nav-up');
+      this.closeNav();
+  } else {
+      if(st + window.innerHeight < document.body.clientHeight) {
+        this.header.classList.replace('nav-up', 'nav-down');
+      }
+  }
+  this.lastScrollTop = st;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var navBar = new NavBar();
+  var toggleNavigationBtn = document.getElementById('toggle-navigation');
+
+  toggleNavigationBtn.onclick = function() {
+    console.log('in click handler')
+    navBar.isOpen === true ? navBar.closeNav() : navBar.openNav()
+  }
+
+  setInterval(function() {
+      if (didScroll) {
+          navBar.handleScroll();
+          didScroll = false;
+      }
+    }, 250);
+});
